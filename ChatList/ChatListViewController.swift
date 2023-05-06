0f0ac97854c8ab40fc87159a02b44fcfc655fb8d
torinunna <torinunna9588@gmail.com>
2023-05-06 16:51:39 +0900

    remove ViewController and create ChatListVC

--- ChatList/ChatListViewController.swift
+++ ChatList/ChatListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChatListViewController.swift
 //  ChatList
 //
 //  Created by YUJIN KWON on 2023/05/06.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
 }
-
